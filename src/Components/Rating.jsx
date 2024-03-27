257fb6173e0c2a91a6da519b91bd1f1d30b66837
Kushal Ghosh <kg2530@it.jgec.ac.in>
2024-03-27 23:32:32 +0530

    new

--- src/Components/Rating.jsx
+++ src/Components/Rating.jsx
@@ -12,7 +12,7 @@ function Rating({ rating_info }) {
       const ratingid = rating_info.result.map((item, index) => index + 1);
       setRatingData({ ratingarray, ratingid });
     }
-    setwidth(window.innerWidth - 200);
+    setwidth(window.innerWidth / 1.5);
     setheight(window.innerHeight / 2);
   }, [rating_info, width, height]);
 
